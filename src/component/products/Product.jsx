d2815e55f8498238a77b904b336ea3c0c147d96e
Olatunji Ayobami Samson <114535570+haywhyvilla@users.noreply.github.com>
2023-02-06 14:36:09 +0100

    new

--- src/component/products/Product.jsx
+++ src/component/products/Product.jsx
@@ -8,11 +8,31 @@ import {
   IconBotton,
 } from "@material-ui/core";
 import { AddShoppingCart } from "material-ui/icons";
+import { IconButton } from "@mui/material";
 
-const Product = () => {
+import useStyles from "./styles";
+
+const Product = ({ product }) => {
   return (
     <card className={classes.root}>
       <CardMedia className={classes.media} image="" title={Product.name} />
+      <CardContent>
+        <div className={classes.cardContent}>
+          <Typography variant="h5" gutterBottom>
+            {Product.name}
+          </Typography>
+          <Typography variant="h5">{Product.price}</Typography>
+        </div>
+        <Typography variant="h2" color="textSecondary">
+          {Product.description}
+        </Typography>
+      </CardContent>
+
+      <CardActions disableSpacing className={classes.CardActions}>
+        <IconButton aria-label="Add to Cart">
+          <AddShoppingCart />
+        </IconButton>
+      </CardActions>
     </card>
   );
 };
---
+++ src/component/products/Product/style.js
@@ -0,0 +1,7 @@
+import { makeStyles } from "@mui/material/styles";
+
+export default makeStyles(() => ({
+  root: {
+    maxWidth: "100%",
+  },
+}));
